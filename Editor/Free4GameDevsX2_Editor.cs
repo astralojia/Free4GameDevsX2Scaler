d964fdeb3992702d2fcda336bad0229bccb28613
astralojia <36993439+astralojia@users.noreply.github.com>
2020-09-12 02:27:45 -0400

    Add files via upload

--- Editor/Free4GameDevsX2_Editor.cs
+++ Editor/Free4GameDevsX2_Editor.cs
@@ -21,7 +21,21 @@ namespace Free4GameDevsX2 {
                 EditorGUILayout.LabelField(":: F4GDX2 SCALING ::");
                 EditorGUILayout.LabelField(" Free for all indie developers to use in their games.");
                 EditorGUILayout.LabelField(" Open Source / CC0 (See License.txt)");
-          
+
+                EditorGUILayout.BeginHorizontal("box");
+                EditorGUILayout.LabelField("Output Filename Addon: ");
+                F4GDX2.outputNameAddon = EditorGUILayout.TextField(F4GDX2.outputNameAddon);
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal("box");
+                EditorGUILayout.LabelField("Soften: ");
+                F4GDX2.SoftenIt = EditorGUILayout.Toggle(F4GDX2.SoftenIt);
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal("box");
+                EditorGUILayout.LabelField("Thicker Lines: ");
+                F4GDX2.ThickerLines = EditorGUILayout.Toggle(F4GDX2.ThickerLines);
+                EditorGUILayout.EndHorizontal();
 
                 EditorGUILayout.LabelField("Scale Amount: ");
                 F4GDX2.scaleAmount = (Free4GameDevsX2.ScaleAmount)EditorGUILayout.EnumPopup(F4GDX2.scaleAmount);
